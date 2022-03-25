befea83200b3aeacf803b6f93f87c3597c31ba32
Claudia Teng <sydeng102@gmail.com>
2022-03-25 09:11:43 +0800

    add works card list

---
+++ src/app/component/works/works.ts
@@ -0,0 +1,20 @@
+export const works = [
+  {
+    title: 'Title',
+    intro: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam autem impedit, veritatis eum quibusdam reprehenderit hic sapiente accusantium atque. Earum ut a ipsam saepe vero illum quas doloribus dolorem animi.',
+    imgPath: 'assets/home.jpg',
+    url: 'https://www.google.com'
+  },
+  {
+    title: 'Title',
+    intro: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam autem impedit, veritatis eum quibusdam reprehenderit hic sapiente accusantium atque. Earum ut a ipsam saepe vero illum quas doloribus dolorem animi.',
+    imgPath: 'assets/home.jpg',
+    url: 'https://www.google.com'
+  },
+  {
+    title: 'Title',
+    intro: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam autem impedit, veritatis eum quibusdam reprehenderit hic sapiente accusantium atque. Earum ut a ipsam saepe vero illum quas doloribus dolorem animi.',
+    imgPath: 'assets/home.jpg',
+    url: 'https://www.google.com'
+  }
+]
