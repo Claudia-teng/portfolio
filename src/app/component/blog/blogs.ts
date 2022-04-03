95f73d85b82aba9d230b54154cf2e11d805d568a
Claudia-teng <sydeng102@gmail.com>
2022-04-03 19:50:05 +0800

    change blog images

--- src/app/component/blog/blogs.ts
+++ src/app/component/blog/blogs.ts
@@ -4,15 +4,15 @@ export const blogs = [
     date: '2022/04/03',
     title: 'JavaScript - OOP | 物件導向就是在做模型！',
     intro: 'OOP是什麼？為什麼要學OOP？物件導向就是在做模型？到底要怎麼用～',
-    imgPath: 'assets/blog/5/5-1.jpg',
-    filePath: 'assets/blog/5/5.md'
+    imgPath: 'assets/blog/6/6-1.png',
+    filePath: 'assets/blog/6/6.md'
   },
   {
     id: 5,
     date: '2022/04/02',
     title: 'JavaScript - Prototype | 原型鏈是怎麼一回事？！',
     intro: '我們平常在使用array裡面的method，他們是怎麼來的？為什麼每一個array都可以使用map()、filter()的method？',
-    imgPath: 'assets/blog/5/5-1.jpg',
+    imgPath: 'assets/blog/5/5-1.png',
     filePath: 'assets/blog/5/5.md'
   },
   {
@@ -20,7 +20,7 @@ export const blogs = [
     date: '2022/04/02',
     title: 'JavaScript - Closure | Closure是記憶的小盒子！',
     intro: 'Closure就是當這個function在外部（脫離了lexical scope）被執行的時候，他仍然會記得原本的lexical scope是什麼。',
-    imgPath: 'assets/blog/4/4-1.jpg',
+    imgPath: 'assets/blog/4/4-1.png',
     filePath: 'assets/blog/4/4.md'
   },
   {
@@ -28,7 +28,7 @@ export const blogs = [
     date: '2022/04/02',
     title: 'JavaScript - Function | Function也是Object？',
     intro: '為什麼JavaScript的Function可以被當成物件？這個物件的特性，又會如何影響我們使用Function？',
-    imgPath: 'assets/blog/3/3-1.jpg',
+    imgPath: 'assets/blog/3/3-1.png',
     filePath: 'assets/blog/3/3.md'
   },
   {
@@ -36,7 +36,7 @@ export const blogs = [
     date: '2022/04/01',
     title: 'Data Structure - Hash Tables | 一個坑不只有一個蘿蔔！JavaScript的Hash Tables是什麼？',
     intro: 'Hash Tables在不同語言當中，有不同的實現方式，而在JavaScript當中，Hash Tables這個資料結構就是以Object的方式呈現！',
-    imgPath: 'assets/blog/2/2-1.jpg',
+    imgPath: 'assets/blog/2/2-1.png',
     filePath: 'assets/blog/2/2.md'
   },
   {
@@ -44,7 +44,7 @@ export const blogs = [
     date: '2022/03/28',
     title: 'Data Structure - Array | 用JavaScript實作Array資料結構',
     intro: 'Array資料結構其實也能透過JavaScript自己實作！此篇會介紹怎麼用Object來實作Array，以及自己寫出一些常見的Array Method。',
-    imgPath: 'assets/blog/1/1-1.jpg',
+    imgPath: 'assets/blog/1/1-1.png',
     filePath: 'assets/blog/1/1.md'
   }
 ]
