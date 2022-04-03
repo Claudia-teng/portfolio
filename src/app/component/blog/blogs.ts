e64eb6a91502cafc58704296b2b2fd3b132d6c76
Claudia-teng <sydeng102@gmail.com>
2022-04-03 19:15:32 +0800

    add blog

--- src/app/component/blog/blogs.ts
+++ src/app/component/blog/blogs.ts
@@ -1,27 +1,19 @@
 export const blogs = [
   {
-    id: 1,
-    date: '2022/03/28',
-    title: 'Data Structure - Array | 用JavaScript實作Array資料結構',
-    intro: 'Array資料結構其實也能透過JavaScript自己實作！此篇會介紹怎麼用Object來實作Array，以及自己寫出一些常見的Array Method。',
-    imgPath: 'assets/blog/1/1-1.jpg',
-    filePath: 'assets/blog/1/1.md'
+    id: 6,
+    date: '2022/04/03',
+    title: 'JavaScript - OOP | 物件導向就是在做模型！',
+    intro: 'OOP是什麼？為什麼要學OOP？物件導向就是在做模型？到底要怎麼用～',
+    imgPath: 'assets/blog/5/5-1.jpg',
+    filePath: 'assets/blog/5/5.md'
   },
   {
-    id: 2,
-    date: '2022/04/01',
-    title: 'Data Structure - Hash Tables | 一個坑不只有一個蘿蔔！JavaScript的Hash Tables是什麼？',
-    intro: 'Hash Tables在不同語言當中，有不同的實現方式，而在JavaScript當中，Hash Tables這個資料結構就是以Object的方式呈現！',
-    imgPath: 'assets/blog/2/2-1.jpg',
-    filePath: 'assets/blog/2/2.md'
-  },
-  {
-    id: 3,
+    id: 5,
     date: '2022/04/02',
-    title: 'JavaScript - Function | Function也是Object？',
-    intro: '為什麼JavaScript的Function可以被當成物件？這個物件的特性，又會如何影響我們使用Function？',
-    imgPath: 'assets/blog/3/3-1.jpg',
-    filePath: 'assets/blog/3/3.md'
+    title: 'JavaScript - Prototype | 原型鏈是怎麼一回事？！',
+    intro: '我們平常在使用array裡面的method，他們是怎麼來的？為什麼每一個array都可以使用map()、filter()的method？',
+    imgPath: 'assets/blog/5/5-1.jpg',
+    filePath: 'assets/blog/5/5.md'
   },
   {
     id: 4,
@@ -32,11 +24,27 @@ export const blogs = [
     filePath: 'assets/blog/4/4.md'
   },
   {
-    id: 5,
+    id: 3,
     date: '2022/04/02',
-    title: 'JavaScript - Prototype | 原型鏈是怎麼一回事？！',
-    intro: '我們平常在使用array裡面的method，他們是怎麼來的？為什麼每一個array都可以使用map()、filter()的method？',
-    imgPath: 'assets/blog/5/5-1.jpg',
-    filePath: 'assets/blog/5/5.md'
+    title: 'JavaScript - Function | Function也是Object？',
+    intro: '為什麼JavaScript的Function可以被當成物件？這個物件的特性，又會如何影響我們使用Function？',
+    imgPath: 'assets/blog/3/3-1.jpg',
+    filePath: 'assets/blog/3/3.md'
+  },
+  {
+    id: 2,
+    date: '2022/04/01',
+    title: 'Data Structure - Hash Tables | 一個坑不只有一個蘿蔔！JavaScript的Hash Tables是什麼？',
+    intro: 'Hash Tables在不同語言當中，有不同的實現方式，而在JavaScript當中，Hash Tables這個資料結構就是以Object的方式呈現！',
+    imgPath: 'assets/blog/2/2-1.jpg',
+    filePath: 'assets/blog/2/2.md'
+  },
+  {
+    id: 1,
+    date: '2022/03/28',
+    title: 'Data Structure - Array | 用JavaScript實作Array資料結構',
+    intro: 'Array資料結構其實也能透過JavaScript自己實作！此篇會介紹怎麼用Object來實作Array，以及自己寫出一些常見的Array Method。',
+    imgPath: 'assets/blog/1/1-1.jpg',
+    filePath: 'assets/blog/1/1.md'
   }
 ]
